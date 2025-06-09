df1ce5caccb75817617e7b13145067f401bf0dff
yass-011 <yasmin.moraes.lobo@escola.pr.gov.br>
2025-06-09 10:54:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'História',
+    'Quem foi o principal líder da Guerra dos Canudos?',
+    'Antônio Conselheiro'
+)
+
+criaCartao(
+    'História',
+    'Quem declarou guerra à Primeira Guerra Mundial?',
+    'Áustria-Hungria foi a primeira a declarar guerra, ao Sérvia, após o assassinato do arquiduque Francisco Ferdinando em Sarajevo'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o país mais forte da Primeira Guerra Mundial?',
+    'O país mais forte da Primeira Guerra Mundial era a Alemanha'
+)
+criaCartao(
+    'História',
+    'Qual foi o causador da Segunda Guerra Mundial?',
+    'O revanchismo alemão, a expansão nazifascista e a invasão da Polônia.'
+)
+
+criaCartao(
+    'História',
+    'O que decretou o fim da Segunda Guerra Mundial?',
+    'rendição incondicional da Alemanha Nazista'
+)
+
+criaCartao(
+    'História',
+    'O que é o plano de Marshall?',
+    'foi uma ajuda financeira dos Estados Unidos para reconstruir a Europa após o final da Segunda Guerra Mundial'
+)
+criaCartao(
+    'História',
+    'O que ocorreu em 1945?',
+    'a Segunda Guerra Mundial chegou ao fim, com a rendição da Alemanha e do Japão'
+)
